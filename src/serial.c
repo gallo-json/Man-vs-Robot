e7a0574c063baf4225fabcf12c9ee4af06c2263e
gallo-json <gallojsantiago@gmail.com>
2020-08-31 09:15:31 -0500

    clean

--- src/serial.c
+++ src/serial.c
@@ -21,55 +21,47 @@ void main(void) {
                         /* Open in blocking mode,read will wait              */
                             
     if(fd == -1)						/* Error Checking */
-            printf("\n  Error! in Opening ttyUSB0  ");
+        printf("\n  Error! in Opening ttyUSB0  ");
     else
-            printf("\n  ttyUSB0 Opened Successfully ");
+        printf("\n  ttyUSB0 Opened Successfully ");
 
 
-/*---------- Setting the Attributes of the serial port using termios structure --------- */
+    /*---------- Setting the Attributes of the serial port using termios structure --------- */
 
-struct termios SerialPortSettings;	/* Create the structure                          */
+    struct termios SerialPortSettings;	/* Create the structure                          */
 
-tcgetattr(fd, &SerialPortSettings);	/* Get the current attributes of the Serial port */
+    tcgetattr(fd, &SerialPortSettings);	/* Get the current attributes of the Serial port */
 
-/* Setting the Baud rate */
-cfsetispeed(&SerialPortSettings,B9600); /* Set Read  Speed as 9600                       */
-cfsetospeed(&SerialPortSettings,B9600); /* Set Write Speed as 9600                       */
+    /* Setting the Baud rate */
+    cfsetispeed(&SerialPortSettings,B9600); /* Set Read  Speed as 9600                       */
+    cfsetospeed(&SerialPortSettings,B9600); /* Set Write Speed as 9600                       */
 
-/* 8N1 Mode */
-SerialPortSettings.c_cflag &= ~PARENB;   /* Disables the Parity Enable bit(PARENB),So No Parity   */
-SerialPortSettings.c_cflag &= ~CSTOPB;   /* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
-SerialPortSettings.c_cflag &= ~CSIZE;	 /* Clears the mask for setting the data size             */
-SerialPortSettings.c_cflag |=  CS8;      /* Set the data bits = 8                                 */
+    /* 8N1 Mode */
+    SerialPortSettings.c_cflag &= ~PARENB;   /* Disables the Parity Enable bit(PARENB),So No Parity   */
+    SerialPortSettings.c_cflag &= ~CSTOPB;   /* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
+    SerialPortSettings.c_cflag &= ~CSIZE;	 /* Clears the mask for setting the data size             */
+    SerialPortSettings.c_cflag |=  CS8;      /* Set the data bits = 8                                 */
 
-SerialPortSettings.c_cflag &= ~CRTSCTS;       /* No Hardware flow Control                         */
-SerialPortSettings.c_cflag |= CREAD | CLOCAL; /* Enable receiver,Ignore Modem Control lines       */ 
-
-
-SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          /* Disable XON/XOFF flow control both i/p and o/p */
-SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);  /* Non Cannonical mode                            */
-
-SerialPortSettings.c_oflag &= ~OPOST;/*No Output Processing*/
-
-/* Setting Time outs */
-SerialPortSettings.c_cc[VMIN] = 10; /* Read at least 10 characters */
-SerialPortSettings.c_cc[VTIME] = 0; /* Wait indefinetly   */
+    SerialPortSettings.c_cflag &= ~CRTSCTS;       /* No Hardware flow Control                         */
+    SerialPortSettings.c_cflag |= CREAD | CLOCAL; /* Enable receiver,Ignore Modem Control lines       */ 
 
 
-if((tcsetattr(fd,TCSANOW,&SerialPortSettings)) != 0) /* Set the attributes to the termios structure*/
-    printf("\n  ERROR ! in Setting attributes");
-else
-    printf("\n  BaudRate = 9600 \n  StopBits = 1 \n  Parity   = none");
+    SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          /* Disable XON/XOFF flow control both i/p and o/p */
+    SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);  /* Non Cannonical mode                            */
+
+    SerialPortSettings.c_oflag &= ~OPOST;/*No Output Processing*/
+
+    if((tcsetattr(fd,TCSANOW,&SerialPortSettings)) != 0) /* Set the attributes to the termios structure*/
+        printf("\n  ERROR ! in Setting attributes");
+    else
+        printf("\n  BaudRate = 9600 \n  StopBits = 1 \n  Parity   = none");
+        
+    tcflush(fd, TCIFLUSH);   /* Discards old data in the rx buffer            */
+
+    char *msg = "#0 P1500\r";
+    write(fd, msg, strlen(msg));
     
-    /*------------------------------- Read data from serial port -----------------------------*/
+    printf("\n +----------------------------------+\n\n\n");
 
-tcflush(fd, TCIFLUSH);   /* Discards old data in the rx buffer            */
-
-
-write(fd, "#0 P1500\r", sizeof("#0 P1500\r"));
-
-printf("\n +----------------------------------+\n\n\n");
-
-close(fd); /* Close the serial port */
-
+    close(fd); /* Close the serial port */
 }
