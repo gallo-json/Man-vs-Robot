d7fdb87e589e315c861865ee803e4076382cc7a8
gallo-json <gallojsantiago@gmail.com>
2020-09-02 14:40:32 -0500

    forgot the selfs python OOP is bad

--- src/ser.py
+++ src/ser.py
@@ -4,12 +4,12 @@ class Ser:
     def __init__(self):
         sp = serial.Serial('/dev/ttyUSB0', 9600)
 
-    def move(motor, deg):
+    def move(self, motor, deg):
         self.sp.write(f'#{motor} P{deg}\r'.encode())
 
-    def idle():
+    def idle(self):
         for i in range(6):
             self.move(i, 1500)
 
-    def cl():
+    def cl(self):
         self.sp.close()
